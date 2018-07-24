ef4da60d41a41bcc0b5c9a86a36001a78750fb54
basecamp <jmorgan@basecampcodingacademy.org>
2018-07-24 11:07:37 -0500

    Added create_item_dictionary that turns my list into a dictionay with the necessary values

--- core.py
+++ core.py
@@ -4,3 +4,17 @@ def create_item_list(inventory_info):
         product = info.split('\n')
         item_list.append(product)
     return item_list
+
+
+def create_item_dictionary(item_list):
+    item_dictionary = {}
+    for product in item_list:
+        items = product.split(',')
+        key = items[0]
+        value = {
+            'In-stock': int(items[1]),
+            'Rent': float(items[2]),
+            'Value': float(items[3])
+        }
+        item_dictionary[key] = value
+    return item_dictionary
