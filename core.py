95b878f7fd54b3c44b0d38016434f2a28173f47b
basecamp <jmorgan@basecampcodingacademy.org>
2018-07-24 13:20:04 -0500

    added the functions remove_from_stock, add_to_stock, and create_file_string along with tests for each one.

--- core.py
+++ core.py
@@ -10,3 +10,20 @@ def create_item_dictionary(item_list):
         }
         item_dictionary[key] = value
     return item_dictionary
+
+
+def remove_from_stock(inventory, item_name):
+    inventory[item_name]['In-stock'] -= 1
+    return inventory
+
+
+def add_to_stock(inventory, item_name):
+    inventory[item_name]['In-stock'] += 1
+    return inventory
+
+
+def create_file_string(inventory):
+    file_string = 'item, in-stock, rent, replacement'
+    for key in inventory:
+        file_string += f"\n{key}, {inventory[key]['In-stock']}, {inventory[key]['Rent']}, {inventory[key]['Value']}"
+    return file_string
--- test_core.py
+++ test_core.py
@@ -23,3 +23,100 @@ def test_create_item_dictionary():
             'Value': 20.0
         }
     }
+
+
+def test_remove_from_stock():
+    inventory = {
+        'laptop': {
+            'In-stock': 16,
+            'Rent': 12.0,
+            'Value': 40.0
+        },
+        'flat-screen': {
+            'In-stock': 1,
+            'Rent': 50.0,
+            'Value': 100.0
+        },
+        'table': {
+            'In-stock': 4,
+            'Rent': 10.0,
+            'Value': 20.0
+        }
+    }
+    assert remove_from_stock(inventory, 'laptop') == {
+        'laptop': {
+            'In-stock': 15,
+            'Rent': 12.0,
+            'Value': 40.0
+        },
+        'flat-screen': {
+            'In-stock': 1,
+            'Rent': 50.0,
+            'Value': 100.0
+        },
+        'table': {
+            'In-stock': 4,
+            'Rent': 10.0,
+            'Value': 20.0
+        }
+    }
+
+
+def test_add_to_stock():
+    inventory = {
+        'laptop': {
+            'In-stock': 16,
+            'Rent': 12.0,
+            'Value': 40.0
+        },
+        'flat-screen': {
+            'In-stock': 1,
+            'Rent': 50.0,
+            'Value': 100.0
+        },
+        'table': {
+            'In-stock': 4,
+            'Rent': 10.0,
+            'Value': 20.0
+        }
+    }
+    assert add_to_stock(inventory, 'flat-screen') == {
+        'laptop': {
+            'In-stock': 16,
+            'Rent': 12.0,
+            'Value': 40.0
+        },
+        'flat-screen': {
+            'In-stock': 2,
+            'Rent': 50.0,
+            'Value': 100.0
+        },
+        'table': {
+            'In-stock': 4,
+            'Rent': 10.0,
+            'Value': 20.0
+        }
+    }
+
+
+def test_create_file_string():
+    inventory = {
+        'laptop': {
+            'In-stock': 16,
+            'Rent': 12.0,
+            'Value': 40.0
+        },
+        'flat-screen': {
+            'In-stock': 1,
+            'Rent': 50.0,
+            'Value': 100.0
+        },
+        'table': {
+            'In-stock': 4,
+            'Rent': 10.0,
+            'Value': 20.0
+        }
+    }
+    assert create_file_string(
+        inventory
+    ) == 'item, in-stock, rent, replacement\nlaptop, 16, 12.0, 40.0\nflat-screen, 1, 50.0, 100.0\ntable, 4, 10.0, 20.0'
